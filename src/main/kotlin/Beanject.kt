a3e65781e690e0e45c38496f3d2f7f239f12c721
Tóth László <dev@tothlp.hu>
2024-05-10 00:18:38 +0200

    feat: add Beanject

---
+++ src/main/kotlin/Beanject.kt
@@ -0,0 +1,32 @@
+object Beanject {
+
+	val beanContainer = mutableMapOf<String, Any>()
+
+	fun getBeanByName(name: String): Any? {
+		return beanContainer[name] ?: throw IllegalArgumentException("No bean found with name $name")
+	}
+
+	inline fun <reified T> getBean(name: String? = null): T {
+		if (beanContainer.none { it.value is T }) throw IllegalArgumentException("No bean found for type ${T::class.simpleName}")
+		return if (!name.isNullOrBlank()) getBeanByName(name) as T
+		else beanContainer.values.first { it is T } as T
+	}
+
+	private fun registerBean(name: String, bean: Any) {
+		beanContainer[name] = bean
+	}
+
+	class BeanDefinitionDsl()
+
+	fun BeanDefinitionDsl.bean(name: String, beanCreator: BeanDefinitionDsl.() -> Any): Any {
+		val bean = beanCreator()
+		registerBean(name, bean)
+		return bean
+	}
+
+	fun beans(init: BeanDefinitionDsl.() -> Unit): BeanDefinitionDsl {
+		val beans = BeanDefinitionDsl()
+		beans.init()
+		return beans
+	}
+}
